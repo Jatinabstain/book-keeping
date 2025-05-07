4aefa8b44cdf23fe7c1ce35363e85c510ea4cdc0
Jatinabstain <54092377+Jatinabstain@users.noreply.github.com>
2025-05-07 16:54:30 +0530

    blank page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,7 @@
+import { redirect } from "next/navigation";
+
 export default function Home() {
   return (
-    <section className="banner_section min-h-screen flex items-center justify-center">
-      <div className="mx-auto max-w-[1200px] px-8 w-full">
-        
-      </div>
-    </section>
+    redirect("/login")
   );
 }
