1dc1cf0cca514374914937803509aa98d3935df0
Jatinabstain <54092377+Jatinabstain@users.noreply.github.com>
2025-05-09 18:02:24 +0530

    update

--- src/app/dashboard/insights/page.tsx
+++ src/app/dashboard/insights/page.tsx
@@ -1,7 +1,57 @@
+"use client";
+
+import { useEffect, useRef } from "react";
+import { useState } from "react";
 import InsightsTabs from "@/components/InsightsTabs";
+import MonthlyAmountChart from "@/components/MonthlyAmountChart";
 import Image from "next/image";
 
 export default function Insights() {
+    const modalRef = useRef<HTMLDivElement>(null);
+
+    const [isModalOpen, setIsModalOpen] = useState(false);
+    const [selectedVendor, setSelectedVendor] = useState<any>(null);
+
+    useEffect(() => {
+        const handleClickOutside = (event: MouseEvent) => {
+            if (modalRef.current && !modalRef.current.contains(event.target as Node)) {
+                setIsModalOpen(false);
+            }
+        };
+
+        if (isModalOpen) {
+            document.addEventListener("mousedown", handleClickOutside);
+        }
+
+        return () => {
+            document.removeEventListener("mousedown", handleClickOutside);
+        };
+    }, [isModalOpen]);
+
+    const handleRowClick = (vendor: any) => {
+        setSelectedVendor(vendor);
+        setIsModalOpen(true);
+    };
+
+    const vendors = [
+        {
+            name: "Pamela Chavez",
+            tag: "COGS Labor",
+            logo: "stripe.svg",
+            change: "+12%",
+            changeColor: "green",
+            amount: "$30,000",
+        },
+        {
+            name: "Pamela Chavez",
+            tag: "COGS Labor",
+            logo: "next.svg",
+            change: "-2%",
+            changeColor: "red",
+            amount: "$10,000",
+        },
+    ];
+
     return (
         <section className="mx-auto max-w-[1400px] px-[15px] w-full pt-8">
             <h1 className="text-xl font-semibold mb-4">Insights</h1>
@@ -20,7 +70,10 @@ export default function Insights() {
                     </div>
                     <div className="content">
                         <h2 className="text-xl font-bold text-primary mb-2">About Recurring Expenses</h2>
-                        <p>The report contains the prepaid subscriptions and recurring expenses we've detected on your books. Click a vendor below to see more details about the monthly trend ana payment nistory.</p>
+                        <p>
+                            The report contains the prepaid subscriptions and recurring expenses we've detected on your books.
+                            Click a vendor below to see more details about the monthly trend and payment history.
+                        </p>
                     </div>
                 </div>
             </div>
@@ -35,75 +88,86 @@ export default function Insights() {
                     </select>
                 </div>
 
-                {/* table vender, Monthly trend, %change, january amount */}
-
-
                 <div className="relative overflow-x-auto shadow-md">
-                    <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
-                        <thead className="text-xs text-gray-700 uppercase bg-gray-100 dark:bg-gray-700 dark:text-gray-400">
+                    <table className="w-full text-sm text-left text-gray-500">
+                        <thead className="text-xs text-gray-700 uppercase bg-gray-100">
                             <tr>
-                                
-                                <th scope="col" className="px-6 py-3">
-                                    Vendor
-                                </th>
-                                <th scope="col" className="px-6 py-3">
-                                    Monthly Trend
-                                </th>
-                                <th scope="col" className="px-6 py-3">
-                                    % Change
-                                </th>
-                                <th scope="col" className="px-6 py-3">
-                                    January Amount
-                                </th>
+                                <th className="px-6 py-3 w-1/2">Vendor</th>
+                                <th className="px-6 py-3">Monthly Trend</th>
+                                <th className="px-6 py-3">% Change</th>
+                                <th className="px-6 py-3">January Amount</th>
                             </tr>
                         </thead>
                         <tbody>
-                            <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 border-gray-200 hover:bg-gray-50">
-                                <th scope="row" className="flex items-center px-6 py-4 text-gray-900 whitespace-nowrap dark:text-white">
-                                    <img className="w-10 h-10 rounded-full" src="/docs/images/people/profile-picture-1.jpg" alt="Jese image" />
+                            {vendors.map((vendor, index) => (
+                                <tr
+                                    key={index}
+                                    className="bg-white border-b border-gray-200 cursor-pointer hover:bg-gray-50"
+                                    onClick={() => handleRowClick(vendor)}
+                                >
+                                    <th scope="row" className="flex items-center px-6 py-4 font-medium text-gray-900 whitespace-nowrap">
+                                        <Image src={vendor.logo} width={40} height={40} alt={vendor.name} />
                                         <div className="ps-3">
-                                            <div className="text-base font-semibold">Neil Sims</div>
-                                            <div className="font-normal text-gray-500">neil.sims@flowbite.com</div>
+                                            <div className="text-base font-semibold">{vendor.name}</div>
+                                            <div className="font-normal text-gray-500">{vendor.tag}</div>
                                         </div>
-                                </th>
-                                <td className="px-6 py-4">
-                                    React Developer
-                                </td>
-                                <td className="px-6 py-4">
-                                    <div className="flex items-center">
-                                        <div className="h-2.5 w-2.5 rounded-full bg-green-500 me-2"></div> Online
-                                    </div>
-                                </td>
-                                <td className="px-6 py-4">
-                                    <a href="#" className="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit user</a>
-                                </td>
-                            </tr>
-                            <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 border-gray-200 hover:bg-gray-50 dark:hover:bg-gray-600">
-                                
-                                <th scope="row" className="flex items-center px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                                    <img className="w-10 h-10 rounded-full" src="/docs/images/people/profile-picture-3.jpg" alt="Jese image" />
-                                        <div className="ps-3">
-                                            <div className="text-base font-semibold">Bonnie Green</div>
-                                            <div className="font-normal text-gray-500">bonnie@flowbite.com</div>
+                                    </th>
+                                    <td className="px-6 py-4 font-medium text-black">
+                                        <MonthlyAmountChart />
+                                    </td>
+                                    <td className="px-6 py-4">
+                                        <div className="flex items-center">
+                                            <span className={`text-sm font-semibold text-${vendor.changeColor}-600`}>
+                                                <i className={`fi fi-rr-arrow-small-${vendor.changeColor === "green" ? "up" : "down"} text-xl align-sub`} />
+                                                {" "}{vendor.change}
+                                            </span>
                                         </div>
-                                </th>
-                                <td className="px-6 py-4">
-                                    Designer
-                                </td>
-                                <td className="px-6 py-4">
-                                    <div className="flex items-center">
-                                        <div className="h-2.5 w-2.5 rounded-full bg-green-500 me-2"></div> Online
-                                    </div>
-                                </td>
-                                <td className="px-6 py-4">
-                                    <a href="#" className="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit user</a>
-                                </td>
-                            </tr>
+                                    </td>
+                                    <td className="px-6 py-4">
+                                        <span className="font-medium text-black">{vendor.amount}</span>
+                                    </td>
+                                </tr>
+                            ))}
                         </tbody>
                     </table>
                 </div>
-
             </div>
+
+            {/* Modal */}
+            {isModalOpen && selectedVendor && (
+                <div className="fixed inset-0 bg-opacity-50 backdrop-blur-sm flex justify-center items-center z-50 transition-all">
+                    <div ref={modalRef} className="relative bg-white shadow-xl p-5 w-full max-w-lg animate-fade-in border border-gray-300">
+                        <button
+                            onClick={() => setIsModalOpen(false)}
+                            className="absolute top-4 right-4 text-gray-400 hover:text-gray-600 cursor-pointer transition"
+                            aria-label="Close"
+                        >
+                            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none"
+                                viewBox="0 0 24 24" stroke="currentColor">
+                                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12" />
+                            </svg>
+                        </button>
+
+                        {/* Modal Content */}
+                        <div>
+                            <div className="flex gap-3 pb-3">
+                                <Image src={selectedVendor.logo} alt={selectedVendor.name} width={50} height={50} />
+                                <div>
+                                    <h2 className="text-base font-bold text-gray-800">{selectedVendor.name}</h2>
+                                    <p className="text-xs text-gray-500">{selectedVendor.tag}</p>
+                                </div>
+                            </div>
+                            
+                            <div className="border-t pt-4 text-left space-y-2 border-gray-300">
+                                {/* <MonthlyAmountChart /> */}
+                                <p><span className="font-medium">January Amount:</span> {selectedVendor.amount}</p>
+                                <p><span className="font-medium">Trend:</span> <span className={`text-${selectedVendor.changeColor}-600`}>{selectedVendor.change}</span></p>
+                            </div>
+                            
+                        </div>
+                    </div>
+                </div>
+            )}
         </section>
     );
 }
--- src/app/dashboard/tax/page.tsx
+++ src/app/dashboard/tax/page.tsx
@@ -1,7 +1,54 @@
-export default function Tax() {
-    return ( 
-        <section className="mx-auto max-w-[1400px] px-[15px] w-full pt-8">
-            <h1 className="text-xl font-semibold mb-0">Tax</h1>
+import Image from "next/image";
+import Link from "next/link";
+
+export default function TaxPage() {
+    return (
+        <section className="mx-auto max-w-[1400px] px-4 py-8 w-full">
+            <h1 className="text-2xl font-semibold text-gray-800 mb-6">Tax</h1>
+            <div className="bg-white p-4 mb-5 shadow">
+                <div className="flex gap-4 items-center">
+                    <div className="image">
+                        <Image
+                            src="tax.svg"
+                            alt="Recurring Expenses"
+                            width={120}
+                            height={120}
+                        />
+                    </div>
+                    <div className="content">
+                        <h2 className="text-xl font-bold text-primary mb-2">Let's keep working on your taxes</h2>
+                        <p>Please review and complete the sections listed below.</p>
+                    </div>
+                </div>
+            </div>
+
+            <div className="flex lg:flex-row flex-col gap-5">
+                <div className="flex-4/5 bg-white shadow p-5 h-max">
+                    <div className="flex gap-4 justify-between items-center">
+                        <div>
+                            <h2 className="text-xl font-bold text-primary">Tax Profile</h2>
+                            <p className="font-semibold text-[#b11107]"><span className="font-bold">Incomplete.</span> Please complete by <span className="font-bold">31 Jan 2025</span></p>
+                        </div>
+                        <button className="bg-primary h-max text-white px-3 py-1.5 hover:bg-dark-primary hover:text-white transition-all duration-200">
+                            Continue Working
+                        </button>
+                    </div>
+                </div>
+                <div className="flex-1/5 bg-white shadow p-5 h-max">
+                    <div className="flex justify-between items-center">
+                        <h2 className="text-xl font-bold text-primary">Contact Info</h2>
+                        <p className="text-xs text-gray-600">Current</p>
+                    </div>
+                    <hr className="my-3 opacity-10" />
+                    <div className="flex flex-col gap-2 leading-4">
+                        <p className="text-gray-700 mb-0">Entity Name</p>
+                        <p className="font-bold mb-4">C Corpration</p>
+                        <p className="text-gray-700 mb-0">Finacal year start</p>
+                        <p className="font-bold mb-4">January</p>
+                        <Link href="#" className="text-primary font-medium">Request Changes</Link>
+                    </div>
+                </div>
+            </div>
         </section>
-     );
+    );
 }
